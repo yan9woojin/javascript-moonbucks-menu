ca96105c1266ed2fba6afbdf2cc2a8b4b7b1dc7d
yan9woojin <yan9woojin@gmail.com>
2023-05-13 18:13:06 +0900

    feat: 엔터키로 새로운 메뉴를 추가하는 기능 구현

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+const $ = (selector) => document.querySelector(selector);
+
+const menuItemTemplate = (espressoMenuName) => {
+  return `
+    <li class="menu-list-item d-flex items-center py-2">
+      <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+      >
+        수정
+      </button>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+      >
+        삭제
+      </button>
+    </li>
+  `;
+};
+
+function App() {
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const $espressoMenuName = $("#espresso-menu-name").value;
+
+      $("#espresso-menu-list").insertAdjacentHTML(
+        "beforeend",
+        menuItemTemplate($espressoMenuName),
+      );
+    }
+  });
+}
+
+App();
