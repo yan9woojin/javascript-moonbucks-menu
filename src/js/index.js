59b41a1ae9a0f3b1db89834aa800b1a6a27b34cc
yan9woojin <yan9woojin@gmail.com>
2023-05-15 21:26:04 +0900

    feat: 서버에 저장된 메뉴의 품절 상태를 변경하는 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -35,6 +35,21 @@ const MenuApi = {
 
     return response.json();
   },
+
+  async toggleSoldOutMenu(category, menuId) {
+    const response = await fetch(
+      `${BASE_URL}/category/${category}/menu/${menuId}/soldout`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+    if (!response.ok) {
+      console.error("에러가 발생했습니다.");
+    }
+
+    return response.json();
+  },
 };
 
 function App() {
@@ -58,7 +73,7 @@ function App() {
 
   const render = () => {
     const template = this.menu[this.currentCategory]
-      .map((item) => menuItemTemplate(item.id, item.name, item.soldOut))
+      .map((item) => menuItemTemplate(item.id, item.name, item.isSoldOut))
       .join("");
     $("#menu-list").innerHTML = template;
     updateMenuCount();
@@ -137,11 +152,12 @@ function App() {
     }
   };
 
-  const soldOutMenu = (e) => {
+  const soldOutMenu = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
-    this.menu[this.currentCategory][menuId].soldOut =
-      !this.menu[this.currentCategory][menuId].soldOut;
-    store.setLocalStorage(this.menu);
+    await MenuApi.toggleSoldOutMenu(this.currentCategory, menuId);
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory,
+    );
     render();
   };
 
